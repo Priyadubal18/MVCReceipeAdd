bdde986ea5d4460a54d40ecb2fae9ff61a89f495
Priyadubal18 <priya.dubal18@gmai.com>
2019-08-15 23:59:07 -0400

    Add the basic setup

---
+++ client/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class ProductBuyer extends React.Component {
+    constructor() {
+        super();
+    }
+    render() {
+        return <h1>Hello, Priya,Good Job</h1>;
+    }
+
+}
---
+++ server/server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.use(express.static(__dirname + '/../client/dist'));
+app.listen(port);
